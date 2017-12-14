81b0aac61733a3ea2dcd467ea7353c62271e57e7
Trung Nguyen <trung.nguyen@kyanon.digital>
2017-12-14 15:38:25 +0700

    apply datatable

--- assets/coffee/sofitel.coffee
+++ assets/coffee/sofitel.coffee
@@ -54,6 +54,85 @@ $ ->
 				
 				return
 			return
+
+	class AllEvents
+		
+		@init: ->
+			do this.setupDataTables
+			return
+
+		@setupDataTables: ->
+			DATA = [
+				'date_added': '30 Jul 2017, 12:18'
+				'account_holder': 'KHAYA'
+				'event_ref': '02131'
+				'event_name': 'Fast Annual Dinner'
+				'status': 'Planning'
+				'percent': 60
+				'contracted_start': '01 Jan 2018'
+				'contracted_duration': '2 days'
+				'company': 'Lorem Ipsum'
+				'customer_name': 'Sylvia Stay'
+				'customer_email': 'sylvia@gmail.com'
+				'reg': '24/65'
+				'is_active': false
+			,
+				'date_added': '30 Jul 2017, 12:18'
+				'account_holder': 'JILEE'
+				'event_ref': '034534'
+				'event_name': 'Singapore Food Festival'
+				'status': 'Planning'
+				'percent': 40
+				'contracted_start': '01 Jan 2018'
+				'contracted_duration': '2 days'
+				'company': 'Lorem Ipsum'
+				'customer_name': 'Sylvia Stay'
+				'customer_email': 'sylvia@gmail.com'
+				'reg': '12/43'
+				'is_active': false
+			,
+				'date_added': '30 Jul 2017, 12:18'
+				'account_holder': 'JOMA'
+				'event_ref': '13130',
+				'event_name': 'Sunway Conf'
+				'status': 'Pending'
+				'contracted_start': '01 Jan 2018'
+				'contracted_duration': '2 days'
+				'company': 'Lorem Ipsum'
+				'customer_name': 'Sylvia Stay'
+				'customer_email': 'sylvia@gmail.com'
+				'reg': '3/42'
+				'is_active': false
+			,
+				'date_added': '30 Jul 2017, 12:18'
+				'account_holder': 'DINSMITH'
+				'event_ref': '42150',
+				'event_name': 'Time Management Event'
+				'status': 'Live'
+				'contracted_start': '01 Jan 2018'
+				'contracted_duration': '2 days'
+				'company': 'Lorem Ipsum'
+				'customer_name': 'Sylvia Stay'
+				'customer_email': 'sylvia@gmail.com'
+				'reg': '3/21'
+				'is_active': false
+			]
+			$('#tblAllEvents').DataTable
+				data: DATA
+				columns: [
+					{ data: 'date_added' },
+					{ data: 'account_holder' },
+					{ data: 'event_ref' },
+					{ data: 'event_name' },
+					{ data: 'contracted_start' },
+					{ data: 'company' },
+					{ data: 'customer_name' },
+					{ data: 'reg' },
+					{ data: 'is_active' }
+				]
+			return
+
 	do General.init
 	do CreateNewBlock.init
+	do AllEvents.init
 	return
--- assets/js/sofitel.js
+++ assets/js/sofitel.js
@@ -1,6 +1,6 @@
 (function() {
   $(function() {
-    var CreateNewBlock, General;
+    var AllEvents, CreateNewBlock, General;
     General = (function() {
       function General() {}
 
@@ -67,8 +67,104 @@
       return CreateNewBlock;
 
     })();
+    AllEvents = (function() {
+      function AllEvents() {}
+
+      AllEvents.init = function() {
+        this.setupDataTables();
+      };
+
+      AllEvents.setupDataTables = function() {
+        var DATA;
+        DATA = [
+          {
+            'date_added': '30 Jul 2017, 12:18',
+            'account_holder': 'KHAYA',
+            'event_ref': '02131',
+            'event_name': 'Fast Annual Dinner',
+            'status': 'Planning',
+            'percent': 60,
+            'contracted_start': '01 Jan 2018',
+            'contracted_duration': '2 days',
+            'company': 'Lorem Ipsum',
+            'customer_name': 'Sylvia Stay',
+            'customer_email': 'sylvia@gmail.com',
+            'reg': '24/65',
+            'is_active': false
+          }, {
+            'date_added': '30 Jul 2017, 12:18',
+            'account_holder': 'JILEE',
+            'event_ref': '034534',
+            'event_name': 'Singapore Food Festival',
+            'status': 'Planning',
+            'percent': 40,
+            'contracted_start': '01 Jan 2018',
+            'contracted_duration': '2 days',
+            'company': 'Lorem Ipsum',
+            'customer_name': 'Sylvia Stay',
+            'customer_email': 'sylvia@gmail.com',
+            'reg': '12/43',
+            'is_active': false
+          }, {
+            'date_added': '30 Jul 2017, 12:18',
+            'account_holder': 'JOMA',
+            'event_ref': '13130',
+            'event_name': 'Sunway Conf',
+            'status': 'Pending',
+            'contracted_start': '01 Jan 2018',
+            'contracted_duration': '2 days',
+            'company': 'Lorem Ipsum',
+            'customer_name': 'Sylvia Stay',
+            'customer_email': 'sylvia@gmail.com',
+            'reg': '3/42',
+            'is_active': false
+          }, {
+            'date_added': '30 Jul 2017, 12:18',
+            'account_holder': 'DINSMITH',
+            'event_ref': '42150',
+            'event_name': 'Time Management Event',
+            'status': 'Live',
+            'contracted_start': '01 Jan 2018',
+            'contracted_duration': '2 days',
+            'company': 'Lorem Ipsum',
+            'customer_name': 'Sylvia Stay',
+            'customer_email': 'sylvia@gmail.com',
+            'reg': '3/21',
+            'is_active': false
+          }
+        ];
+        $('#tblAllEvents').DataTable({
+          data: DATA,
+          columns: [
+            {
+              data: 'date_added'
+            }, {
+              data: 'account_holder'
+            }, {
+              data: 'event_ref'
+            }, {
+              data: 'event_name'
+            }, {
+              data: 'contracted_start'
+            }, {
+              data: 'company'
+            }, {
+              data: 'customer_name'
+            }, {
+              data: 'reg'
+            }, {
+              data: 'is_active'
+            }
+          ]
+        });
+      };
+
+      return AllEvents;
+
+    })();
     General.init();
     CreateNewBlock.init();
+    AllEvents.init();
   });
 
 }).call(this);
